1402d45af6d7d77d277d1ff1a2fe64d765d26833
vescobart <142045595+vescobart@users.noreply.github.com>
2023-08-18 16:12:22 -0300

    Cambios codigo

--- COde1.R
+++ COde1.R
@@ -2,5 +2,7 @@ library(gitcreds)
 library(usethis)
 create_github_token()
 gitcreds_set()
-
-
+usethis::use_github()
+gh_token_help()
+usethis::git_sitrep()###cod. JC
+usethis::edit_r_environ()###Cod2 JC
